e41cbd7fcfe1eaf8cf2fa2427d04b9c34ebf12d0
Xu Li <aaronleemcgill@gmail.com>
2012-09-12 01:31:25 +0000

    git-svn-id: https://svn.sable.mcgill.ca/mclab/Project@2278 89b902b4-d83f-0410-b942-89f7a419ffdb
    Migrated from Sable/mclab@898c300ca34537043625cd446898e491ac98ad8e

--- languages/Natlab/src/natlab/backends/Fortran/codegen/generateAST.sh
+++ languages/Natlab/src/natlab/backends/Fortran/codegen/generateAST.sh
@@ -1,3 +1,2 @@
 cd ~/Project/languages/Natlab/src
-rm -rf natlab/backends/Fortran/codegen/FortranAST
 java -jar natlab/backends/Fortran/codegen/jastadd2.jar --package=natlab.backends.Fortran.codegen.FortranAST natlab/backends/Fortran/codegen/FortranIR.ast
