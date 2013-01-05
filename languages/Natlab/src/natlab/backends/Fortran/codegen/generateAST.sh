d5a27ccc1e4b53123ae0fecebdcde7bd04c9095a
aaronleemcgill <aaronleemcgill@gmail.com>
2013-01-05 14:28:07 -0500

    modify generateAST.sh, when the Mc2For backend is done, I will put generating Fortran AST command in the mclab build file.
    Migrated from Sable/mclab@b1afb43e7584b23e8fc67227754cdd0727addf76

--- languages/Natlab/src/natlab/backends/Fortran/codegen/generateAST.sh
+++ languages/Natlab/src/natlab/backends/Fortran/codegen/generateAST.sh
@@ -1,3 +1,3 @@
-rm ~/Project/languages/Natlab/src/natlab/backends/Fortran/codegen/FortranAST/*.java
-cd ~/Project/languages/Natlab/src
+rm ~/mclab/languages/Natlab/src/natlab/backends/Fortran/codegen/FortranAST/*.java
+cd ~/mclab/languages/Natlab/src
 java -jar natlab/backends/Fortran/codegen/jastadd2.jar --package=natlab.backends.Fortran.codegen.FortranAST natlab/backends/Fortran/codegen/FortranIR.ast natlab/backends/Fortran/codegen/PrettyPrinter.jadd
