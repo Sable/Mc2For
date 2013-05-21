1ae9904fa1294faff6453636e063d2c9c1b9e226
Xu Li <aaronleemcgill@gmail.com>
2013-05-21 02:22:11 -0400

    add new Fortran back end based on Tamer+ currently, just start with pretty printing the generated code.
    Migrated from Sable/mclab@7655eac693ce0be64839e5aa4c7bf8c0829dec09

---
+++ languages/Natlab/src/natlab/backends/Fortran/MainForTamerPlus.java
@@ -0,0 +1,66 @@
+package natlab.backends.Fortran;
+
+import java.util.Set;
+
+import ast.*;
+
+import natlab.tame.BasicTamerTool;
+import natlab.tame.callgraph.StaticFunction;
+import natlab.tame.tamerplus.analysis.AnalysisEngine;
+import natlab.tame.tamerplus.transformation.TransformationEngine;
+import natlab.tame.valueanalysis.ValueAnalysis;
+import natlab.tame.valueanalysis.ValueFlowMap;
+import natlab.tame.valueanalysis.aggrvalue.AggrValue;
+import natlab.tame.valueanalysis.basicmatrix.BasicMatrixValue;
+import natlab.toolkits.filehandling.GenericFile;
+import natlab.toolkits.path.FileEnvironment;
+
+public class MainForTamerPlus {
+	
+	public static void main(String[] args) {
+		/**
+		 * This main method is just for testing, doesn't follow the convention when passing a 
+		 * file to a program, please replace "fileDir and fileIn" below with your real testing 
+		 * file directory and its name, and you can pass the type info of the input argument 
+		 * to the program, currently, the type info is composed like double&3*3&REAL.
+		 */
+		String fileDir = "/home/xu/for_test/";
+	    String fileIn = fileDir+"bubble.m";
+	    GenericFile gFile = GenericFile.create(fileIn);
+		FileEnvironment env = new FileEnvironment(gFile); //get path environment obj
+		BasicTamerTool tool = new BasicTamerTool();
+		ValueAnalysis<AggrValue<BasicMatrixValue>>  analysis = tool.analyze(args, env);
+		int size = analysis.getNodeList().size();
+		
+		for (int i=0; i<size; i++) {
+			/*
+			 * type inference.
+			 */
+			ValueFlowMap<AggrValue<BasicMatrixValue>> currentOutSet = 
+					analysis.getNodeList().get(i).getAnalysis().getCurrentOutSet();
+			// System.err.println(currentOutSet);
+			/*
+			 * tamer plus analysis.
+			 */
+			StaticFunction function = analysis.getNodeList().get(i).getFunction();
+			// TamerPlusUtils.debugMode();
+			// System.out.println("tamer pretty print: \n"+function.getAst().getPrettyPrinted());
+	        TransformationEngine transformationEngine = TransformationEngine
+	        		.forAST(function.getAst());
+	        AnalysisEngine analysisEngine = transformationEngine
+	        		.getAnalysisEngine();
+	        ASTNode fTree = transformationEngine
+	        		.getTIRToMcSAFIRWithoutTemp().getTransformedTree();
+	        Set<String> remainingVars = analysisEngine
+	        		.getTemporaryVariablesRemovalAnalysis().getRemainingVariablesNames();
+	        System.out.println("\ntamer plus analysis result: \n"+fTree.getPrettyPrinted()+"\n");
+	        // System.err.println("remaining variables: \n"+remainingVars);
+	        /*
+	         * Fortran code generation.
+	         */
+	        System.err.println("pretty print the generated Fortran code.\n");
+	        PrettyPrintFortran prettyPrintFortran = 
+	        		new PrettyPrintFortran((Function)fTree, currentOutSet, remainingVars);
+        }
+	}
+}
---
+++ languages/Natlab/src/natlab/backends/Fortran/OperatorMapping.java
@@ -0,0 +1,73 @@
+package natlab.backends.Fortran;
+
+import java.util.HashMap;
+
+public class OperatorMapping {
+
+	private static HashMap<String, String> FortranBinOperatorMap = 
+			new HashMap<String, String>();
+	private static HashMap<String, String> FortranDirectBuiltinMap = 
+			new HashMap<String, String>();
+	
+	private void makeFortranBinaryOperatorMap() {
+		FortranBinOperatorMap.put("plus", "+");
+		FortranBinOperatorMap.put("minus", "-");
+		FortranBinOperatorMap.put("mtimes", "*");
+		FortranBinOperatorMap.put("mrdivide", "/");
+		FortranBinOperatorMap.put("mldivide", "\\");
+		FortranBinOperatorMap.put("mpower", "**");
+		FortranBinOperatorMap.put("times", "*");
+		FortranBinOperatorMap.put("rdivide", "/");
+		FortranBinOperatorMap.put("ldivide", "\\");
+		FortranBinOperatorMap.put("power", "**");
+		FortranBinOperatorMap.put("and", ".AND.");
+		FortranBinOperatorMap.put("or", ".OR.");
+		FortranBinOperatorMap.put("lt", ".LT.");
+		FortranBinOperatorMap.put("gt", ".GT.");
+		FortranBinOperatorMap.put("le", ".LE.");
+		FortranBinOperatorMap.put("ge", ".GE.");
+		FortranBinOperatorMap.put("eq", ".EQ.");
+		FortranBinOperatorMap.put("ne", ".NE.");
+		FortranBinOperatorMap.put("not", ".NOT.");
+	}
+	
+	private void makeFortranDirectBuiltinMap() {
+		/* 
+		 * TODO create a categorical map here 
+		 */
+		FortranDirectBuiltinMap.put("sqrt", "SQRT");	
+		FortranDirectBuiltinMap.put("sin", "SIN");	
+		FortranDirectBuiltinMap.put("cos", "COS");
+		FortranDirectBuiltinMap.put("sum", "SUM");
+		FortranDirectBuiltinMap.put("size", "SIZE");
+		FortranDirectBuiltinMap.put("length", "SIZE");
+		FortranDirectBuiltinMap.put("exp", "EXP");
+		FortranDirectBuiltinMap.put("transpose", "TRANSPOSE");
+		FortranDirectBuiltinMap.put("ceil", "CEILING");
+		FortranDirectBuiltinMap.put("abs", "ABS");
+		FortranDirectBuiltinMap.put("round", "INT");
+	}
+	
+	public OperatorMapping() {
+		makeFortranBinaryOperatorMap();
+		makeFortranDirectBuiltinMap();
+	}
+	
+	public Boolean isFortranBinOperator(String operator) {
+		if (FortranBinOperatorMap.containsKey(operator)) return true;
+		else return false;
+	}
+	
+	public String getFortranBinOpMapping(String operator) {
+		return FortranBinOperatorMap.get(operator);
+	}
+	
+	public Boolean isFortranDirectBuiltin(String builtinName) {
+		if (FortranDirectBuiltinMap.containsKey(builtinName)) return true;
+		else return false;
+	}
+	
+	public String getFortranDirectBuiltinMapping (String builtinName) {
+		 return FortranDirectBuiltinMap.get(builtinName);		
+	}
+}
