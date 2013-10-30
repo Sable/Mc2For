69fbdd964ee72e2aa8c6298a32cbe432245c40c2
Xu Li <aaronleemcgill@gmail.com>
2013-10-29 23:16:24 -0400

    add -codegen and -nocheck to options in natlab.main there is one thing to note, the arguments we pass to tamer and back end, like dbl&1*1, should be embraced in double quotes, like "dbl&1*1", if not, ampersand (&) means command end in terminal...should we find a better delimiter?
    Migrated from Sable/mclab@1b81e106991cb3cf778824d201ec0b3eefced4ca

--- languages/Natlab/src/natlab/backends/Fortran/codegen_readable/Main_readable.java
+++ languages/Natlab/src/natlab/backends/Fortran/codegen_readable/Main_readable.java
@@ -9,6 +9,7 @@ import java.util.Set;
 import ast.ASTNode;
 import ast.*;
 
+import natlab.options.Options;
 import natlab.tame.BasicTamerTool;
 import natlab.tame.callgraph.StaticFunction;
 import natlab.tame.tamerplus.analysis.AnalysisEngine;
@@ -108,6 +109,97 @@ public class Main_readable {
 			} catch(IOException e) {
 				System.err.println(e);
 			}
+        }
+	}
+	
+	public static void compile(Options options) {
+		FileEnvironment fileEnvironment = new FileEnvironment(options); //get path/files
+
+		//arguments - TODO for now just parse them as inputs
+		String args = "double&1*1"; //start with the default
+		if (options.arguments() != null && options.arguments().length() > 0){
+			args = options.arguments();
+		}
+		
+		// TODO now it's for testing...
+		String[] argsList = {args};
+		
+		BasicTamerTool tool = new BasicTamerTool();
+		ValueAnalysis<AggrValue<BasicMatrixValue>>  analysis = tool.analyze(argsList, fileEnvironment);
+		int size = analysis.getNodeList().size();
+		
+		// preprocess to get all the names of the user defined functions in the program.
+		Set<String> userDefinedFunctions = new HashSet<String>();
+		for (int i = 0; i < size; i++) {
+			String functionName = analysis.getNodeList().get(i).getFunction().getName();
+			if (!functionName.equals(fileEnvironment.getMainFile().getName().replace(".m", ""))) {
+				userDefinedFunctions.add(functionName);
+			}
+		}
+		
+		/* 
+		 * run tamer plus analysis first, then using the AST from tamer plus 
+		 * to generate fortran AST and let the AST pretty print itself. 
+		 */
+		for (int i = 0; i < size; i++) {
+			/*
+			 * type inference.
+			 */
+			ValueFlowMap<AggrValue<BasicMatrixValue>> currentOutSet = 
+					analysis.getNodeList().get(i).getAnalysis().getCurrentOutSet();
+			// System.err.println(currentOutSet);
+			/*
+			 * tamer plus analysis.
+			 */
+			StaticFunction function = analysis.getNodeList().get(i).getFunction();
+			// TamerPlusUtils.debugMode();
+			// System.out.println("tamer pretty print: \n"+function.getAst().getPrettyPrinted());
+	        TransformationEngine transformationEngine = TransformationEngine
+	        		.forAST(function.getAst());
+	        AnalysisEngine analysisEngine = transformationEngine
+	        		.getAnalysisEngine();
+	        @SuppressWarnings("rawtypes")
+	        ASTNode fTree = transformationEngine
+	        		.getTIRToMcSAFIRWithoutTemp().getTransformedTree();
+	        Set<String> remainingVars = analysisEngine
+	        		.getTemporaryVariablesRemovalAnalysis().getRemainingVariablesNames();
+	        System.err.println("\ntamer plus analysis result: \n" 
+	        		+ fTree.getPrettyPrinted() + "\n");
+	        // System.err.println("remaining variables: \n"+remainingVars);
+	        
+	        /*
+	         * Fortran code generation.
+	         */
+	        System.err.println("pretty print the generated Fortran code:");
+	        Subprogram subprogram = FortranCodeASTGenerator.generateFortran(
+	        		(Function)fTree, 
+	        		currentOutSet, 
+	        		remainingVars, 
+	        		fileEnvironment.getMainFile().getName().replace(".m", ""));
+	        StringBuffer sb = new StringBuffer();
+	        String currentFunction = subprogram.getProgramTitle().getProgramName();
+	        if (!currentFunction.equals(fileEnvironment.getMainFile().getName().replace(".m", ""))) {
+	        	sb.append("MODULE mod_"+currentFunction+"\n\nCONTAINS\n\n");
+	        	subprogram.pp(sb);
+	        	sb.append("\nEND MODULE");
+	        }
+	        else {
+	        	subprogram.pp(sb);
+	        }
+	        System.out.println(sb);
+			
+	        // write the transformed result to files.
+	        try {
+	        	BufferedWriter out = new BufferedWriter(new FileWriter(
+	        			fileEnvironment.getPwd().getPath()
+	        			+ "/"
+	        			+ function.getName() + ".f95"));
+	        	out.write(sb.toString());
+	        	out.flush();
+	        	out.close();
+	        } catch (IOException e) {
+	        	System.err.println(e);
+	        }
         }
 	}
 }
